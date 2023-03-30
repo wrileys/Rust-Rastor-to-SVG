d78e9cfff680e43a5d9b8f8b0692eba32b31308d
Riley Shuping <top.year9877@fastmail.com>
2023-03-30 15:31:52 -0500

    Initial files with rdp

---
+++ src/edge_detection.rs
@@ -0,0 +1,9 @@
+// src/edge_detection.rs
+use crate::image_to_svg_converter::ImageToSVGConverter;
+use image::{DynamicImage, GenericImageView};
+
+impl ImageToSVGConverter {
+    pub fn sobel_edge_detection(&self, image: &DynamicImage) -> DynamicImage {
+        // Implement Sobel Operator edge detection here.
+    }
+}
